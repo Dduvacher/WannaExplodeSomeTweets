099da51884021dea6f89036f1811314afc8bcc01
Dduvacher <dduvacher@gmail.com>
2017-03-26 19:49:05 +0200

    Setting pixi.js, first use of pixi.js

--- tweeterInteraction.js
+++ tweeterInteraction.js
@@ -1,6 +1,19 @@
 var request = require('request');
 var Twitter = require('twitter');
+var http = require('http');
+var express = require('express');
+var app = express();
 require('dotenv').config();
+
+
+app.get('/tweetwall', function(req, res) {
+    res.render('tweetwallview.ejs');
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -8,6 +21,8 @@ var client = new Twitter({
   bearer_token: process.env.TWITTER_BEARER_TOKEN
 });
 
-client.get('search/tweets', {q: '#pixelart'}, function(error, tweets, response) {
+/*client.get('search/tweets', {q: '#pixelart'}, function(error, tweets, response) {
    console.log(tweets);
-});
+});*/
+
+app.listen(8080);
